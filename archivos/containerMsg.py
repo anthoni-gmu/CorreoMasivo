c302eef4294147ef0ccc65748c4369da9186b7f4
anthoni-gmu <softwaretoni21@gmail.com>
2021-08-13 12:03:15 -0500

    svg

--- archivos/containerMsg.py
+++ archivos/containerMsg.py
@@ -5,18 +5,22 @@ def container(nombre):
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
 	<meta name="x-apple-disable-message-reformatting">
+	<script src="https://kit.fontawesome.com/50872a9b90.js" crossorigin="anonymous"></script>
 	<title></title>
-	<!--[if mso]>
-	<noscript>
-		<xml>
-			<o:OfficeDocumentSettings>
-				<o:PixelsPerInch>96</o:PixelsPerInch>
-			</o:OfficeDocumentSettings>
-		</xml>
-	</noscript>
-	<![endif]-->
+	
 	<style>
-		table, td, div, h1, p {font-family: Arial, sans-serif;}
+		@import url('https://fonts.googleapis.com/css2?family=Khula:wght@300;400;600;700;800&display=swap');
+		table, td, div, h1, p {font-family: 'Khula', sans-serif;}
+		.boton{
+			border: #ee4c50;
+			text-decoration: none;
+			background:#CCCB00  	;
+			padding:15px 30px;
+			border-radius: 45px;
+			color: #026634;
+			font-weight: bold;
+			font-size: 22px;
+		}
 	</style>
 </head>
 <body style="margin:0;padding:0;">
@@ -25,59 +29,78 @@ def container(nombre):
 			<td align="center" style="padding:0;">
 				<table role="presentation" style="width:602px;border-collapse:collapse;border:1px solid #cccccc;border-spacing:0;text-align:left;">
 					<tr>
-						<td align="center" style="padding:40px 0 30px 0;background:#70bbd9;">
-							<img src="https://scontent.flim20-1.fna.fbcdn.net/v/t1.6435-9/103686236_998582540560911_3139396867292022443_n.png?_nc_cat=105&ccb=1-3&_nc_sid=973b4a&_nc_ohc=ohFmuh6SNBcAX_dZXCS&_nc_ht=scontent.flim20-1.fna&oh=e642c0593fc36d3fec54db111bc144c2&oe=6122C276" alt="" width="300" style="height:auto;display:block;" />
+						<td align="center" >
+							<img src="https://raw.githubusercontent.com/anthoni-gmu/CorreoMasivo/main/ANTONI-01.png" alt="" width="660" style="height:auto;display:block;" />
 						</td>
 					</tr>
 					<tr>
 						<td style="padding:36px 30px 42px 30px;">
 							<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
 								<tr>
-									<td style="padding:0 0 36px 0;color:#153643;">
-										<h1 style="font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;">"""+nombre+"""</h1>
-										<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan et dictum, nisi libero ultricies ipsum, posuere neque at erat.</p>
-										<p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://www.example.com" style="color:#ee4c50;text-decoration:underline;">In tempus felis blandit</a></p>
+									<td style="padding:0 0 36px 0;color:#53802F;">
+										<h1 style="font-weight:bold ;font-size:24px;margin:0 0 20px 0;font-family:Arial,sans-serif;text-align: center;">¡Hola Anthoni!</h1>
+										<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;text-align: center;font-size: 23px;">Gracias por confiar en nosotros.</p>
 									</td>
 								</tr>
 								<tr>
-									<td style="padding:0;">
-										<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;">
-											<tr>
-												<td style="width:260px;padding:0;vertical-align:top;color:#153643;">
-													<p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><img src="https://assets.codepen.io/210284/left.gif" alt="" width="260" style="height:auto;display:block;" /></p>
-													<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed. Morbi porttitor, eget accumsan dictum, est nisi libero ultricies ipsum, in posuere mauris neque at erat.</p>
-													<p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://www.example.com" style="color:#ee4c50;text-decoration:underline;">Blandit ipsum volutpat sed</a></p>
-												</td>
-												<td style="width:20px;padding:0;font-size:0;line-height:0;">&nbsp;</td>
-												<td style="width:260px;padding:0;vertical-align:top;color:#153643;">
-													<p style="margin:0 0 25px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><img src="https://assets.codepen.io/210284/right.gif" alt="" width="260" style="height:auto;display:block;" /></p>
-													<p style="margin:0 0 12px 0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;">Morbi porttitor, eget est accumsan dictum, nisi libero ultricies ipsum, in posuere mauris neque at erat. Lorem ipsum dolor sit amet, consectetur adipiscing elit. In tempus adipiscing felis, sit amet blandit ipsum volutpat sed.</p>
-													<p style="margin:0;font-size:16px;line-height:24px;font-family:Arial,sans-serif;"><a href="http://www.example.com" style="color:#ee4c50;text-decoration:underline;">In tempus felis blandit</a></p>
-												</td>
-											</tr>
-										</table>
+									<td align="center" >
+										<img src="https://raw.githubusercontent.com/anthoni-gmu/CorreoMasivo/main/ANTONI-02.png" alt="" width="600" style="height:auto;display:block;" />
+									</td>
+								</tr>
+								<tr>
+									<td style="padding:0; text-align: center;">
+										<i class="fas fa-map-marker-alt" style="color:#3D662A;font-size: 25px;"></i>
+										<p style="font-size:23px;line-height:24px;font-family:Arial,sans-serif;text-align: center;color:#354A20;line-height : 4px;">EN PLENA PANAMERICANA</p>
+										<p style="font-size:23px;line-height:24px;font-family:Arial,sans-serif;text-align: center;color:#3D662A;font-weight: bold;line-height : 4px;">CON SALIDA A LA PLAYA</p>
+										<h1 style="color:#E5960C">CAÑETE</h1>
+										<a href="https://uqr.to/xlbs" class="boton " >CUPÓN AQUÍ</a>
 									</td>
 								</tr>
 							</table>
 						</td>
 					</tr>
 					<tr>
-						<td style="padding:30px;background:#ee4c50;">
+						<td style="padding:10px;background:#026634;">
+							<p style="font-weight: bold; color:#ffffff;text-transform:uppercase;font-size:30px;line-height:4px;text-align: center;word-spacing:10px;letter-spacing:1px">¡APROVECHA Y NO DEJES PASAR</p>
+							<p style="font-weight: bold; color:#ffffff;text-transform:uppercase;font-size:30px;line-height:4px;text-align: center;word-spacing:10px;letter-spacing:1px">ESTA OPORTUNIDAD!</p>
+						</td>
+						
+					</tr>
+					<tr>
+						<td style="padding:30px;background:#ffffff;">
+							<h1 style="color:#4B7A25;font-size:30px;">Términos y condiciones</h1>
+							<p style="color:#32471D;font-size:16px;">
+								Este cupón es propiedad de Inmobiliaria Huertas ubicada en Call. Luis Espejo Nro. 1097 -1093 Int. 803 La Victoria.
+							</p>
+							<p style="color:#32471D;font-size:16px;">
+Los cupones de descuento son de exclusivo uso personal. No está permitido vender, comerciar o adquirir cupones de descuento de ninguna manera sin el permiso de la empresa.
+							</p>
+							<p style="color:#32471D;font-size:16px;">
+En cumplimiento de lo dispuesto por la Ley N° 29733, Ley de Protección de Datos Personales y su reglamento aprobado por Decreto Supremo N° 003-2013-JUS, Huertas Inmobiliaria desea poner en conocimiento que el uso de datos personales de nuestros clientes son de extricto uso comercial interno en vías de ofrecerles calidad de servicio y mejores oportunidades de inversión.
+							</p>
+						</td>
+						
+					</tr>
+					<tr>
+						<td style="padding:30px;background:#026634;">
 							<table role="presentation" style="width:100%;border-collapse:collapse;border:0;border-spacing:0;font-size:9px;font-family:Arial,sans-serif;">
 								<tr>
 									<td style="padding:0;width:50%;" align="left">
 										<p style="margin:0;font-size:14px;line-height:16px;font-family:Arial,sans-serif;color:#ffffff;">
-											&reg; Someone, Somewhere 2021<br/><a href="http://www.example.com" style="color:#ffffff;text-decoration:underline;">Unsubscribe</a>
+											Calle Luis Espejo 1097 Santa Catalina <br> La Victoria
 										</p>
 									</td>
 									<td style="padding:0;width:50%;" align="right">
 										<table role="presentation" style="border-collapse:collapse;border:0;border-spacing:0;">
 											<tr>
 												<td style="padding:0 0 0 10px;width:38px;">
-													<a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/tw_1.png" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
+													<a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://cdn.worldvectorlogo.com/logos/whatsapp-3.svg" alt="Whatsapp" width="38" style="height:auto;display:block;border:0;" /></a>
 												</td>
 												<td style="padding:0 0 0 10px;width:38px;">
-													<a href="http://www.facebook.com/" style="color:#ffffff;"><img src="https://assets.codepen.io/210284/fb_1.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
+													<a href="http://www.twitter.com/" style="color:#ffffff;"><img src="https://cdn.worldvectorlogo.com/logos/instagram-5.svg" alt="Twitter" width="38" style="height:auto;display:block;border:0;" /></a>
+												</td>
+												<td style="padding:0 0 0 10px;width:38px;">
+													<a href="http://www.facebook.com/" style="color:#ffffff;"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1365px-Facebook_f_logo_%282019%29.svg.png" alt="Facebook" width="38" style="height:auto;display:block;border:0;" /></a>
 												</td>
 											</tr>
 										</table>
@@ -85,6 +108,7 @@ def container(nombre):
 								</tr>
 							</table>
 						</td>
+						
 					</tr>
 				</table>
 			</td>
